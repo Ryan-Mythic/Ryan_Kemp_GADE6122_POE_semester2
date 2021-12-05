8ea21c58002cb9a5966b5088d9febbe159321f4c
Ryan-Mythic <91421957+Ryan-Mythic@users.noreply.github.com>
2021-12-05 23:22:36 +0200

    Add files via upload

---
+++ Q2.1/MeleeWeapon.cs
@@ -0,0 +1,25 @@
+﻿using System;                       //Ryan Kemp
+using System.Collections.Generic;   //20100423
+using System.Text;
+
+namespace Task_1
+{
+    abstract class MeleeWeapon : Weapon
+    {
+        public MeleeWeapon(int x, int y, TileType tileType, char symbol) : base(x, y, tileType, symbol)
+        {
+
+        }
+
+        public enum Types
+        {
+            Dagger,
+            LongSword
+        }
+
+        public override void Range() //ReturnRange
+        {
+            return 1;
+        }
+    }
+}
